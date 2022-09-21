519fb2b16a74acf37c2b7a822078f34888cdf0c0
FengZihan3531 <113589804+FengZihan3531@users.noreply.github.com>
2022-09-21 23:40:11 +0800

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -10,17 +10,17 @@ public class test {
     public static void main(String[] args) throws Exception{
         List<Student> sl = input.ReadStudent("D:\\file\\输入1：答辩学生信息表.xls");
         List<Tutor> tl = input.ReadTutor("D:\\file\\输入2：硕博导师和校外专家池.xls");
-        input.init(sl,tl);
+        input.init(sl,tl);  //处理输入
         for (int i = 0; i < tl.size(); i++) {
-            System.out.println(tl.get(i));
+            System.out.println(tl.get(i));  //每个导师分配的任务数量
         }
-        access.start(sl,tl);
+        access.start(sl,tl); // 进行分配
         for (int i = 0; i < sl.size(); i++) {
-            System.out.println(sl.get(i));
+            System.out.println(sl.get(i));  //每个学生被分配后的状态
         }
         for (int i = 0; i < tl.size(); i++) {
-            System.out.println(tl.get(i));
+            System.out.println(tl.get(i));  //每个导师分配后的状态
         }
-        output.createExcel("D:\\file\\输出.xls",sl);
+        output.createExcel("D:\\file\\输出.xls",sl);  //输出结果为excel文件
     }
 }
