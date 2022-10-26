35c7e698f01babaf0e5816aa9f7badac74003c03
dumpling <1670361574@qq.com>
2022-10-27 01:30:18 +0800

    加了一些注释，现在可以输入每天预答辩组数，默认每天5组

--- 答辩与预答辩/src/student.py
+++ 答辩与预答辩/src/student.py
@@ -3,6 +3,14 @@ class Student:
     secondTutor = ""
 
     def __init__(self, num, sId, sName, tutorName, centerName):
+        """
+
+        :param num: 学生在excel中序号
+        :param sId: 学号
+        :param sName: 姓名
+        :param tutorName: 导师姓名
+        :param centerName: 所属中心
+        """
         self.num = num
         self.sId = sId
         self.sName = sName
